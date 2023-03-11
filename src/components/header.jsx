d7b74bd431f803776958256071fef8977ca36089
gonzalo mina <gonzalo.mina@hotmail.com.ar>
2023-03-11 05:11:26 -0300

    arrancamos con el login

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,11 +1,17 @@
-import React from 'react'
-import Filters from './filters'
+import React, { useState } from 'react'
 
 function Header() {
-  
+  const [auth, setAuth] = useState(false)
+
+  const handleAuth = () => {
+    {auth ? setAuth(false) : setAuth(true)}
+  }
   return (
     <div className='header'>
-        <Filters></Filters>
+       { auth 
+       ? <button onClick={handleAuth}>logout</button>
+       : <button onClick={handleAuth}>login</button>} 
+       
     </div>
   )
 }
